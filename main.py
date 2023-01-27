acddcf509cce8ceebc55ea4c2e6b286b60a0a583
Stefano <stefano.rubini@gmail.com>
2023-01-27 13:27:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from st_on_hover_tabs import on_hover_tabs
 from pgs.pg_edit import edit
 from pgs.pg_upload import upload
 
-st.set_page_config(layout="wide")
-st.markdown('<style>' + open('./static/style.css').read() + '</style>', unsafe_allow_html=True)
 #------------------------------------------------------------------------------#
 # Parametri di configurazione                                                  #
 #------------------------------------------------------------------------------#
@@ -18,11 +16,9 @@ rows_to_display = 10
 #------------------------------------------------------------------------------#
 # Inizializzazione e funzioni                                                  #
 #------------------------------------------------------------------------------#
-uploaded_file = None
+st.set_page_config(layout="wide")
+st.markdown('<style>' + open('./static/style.css').read() + '</style>', unsafe_allow_html=True)
 
-#------------------------------------------------------------------------------#
-# Pagina principale                                                            #
-#------------------------------------------------------------------------------#
 st.title("CSV Toools")
 
 with st.sidebar:
@@ -33,6 +29,7 @@ with st.sidebar:
     )
     st.session_state.tabs = tabs
 
+# Pagina Upload
 if tabs =='Upload':
     st.header('{}'.format(tabs))
     upload(file_ext, input_separator, rows_to_display)
@@ -40,10 +37,12 @@ if tabs =='Upload':
     #     tabs = 'Edit'
     #     st.write('ciao ciao')
 
+#Pagina Edit
 elif tabs == 'Edit':
     st.header('{}'.format(tabs))
     edit()
 
+#Pagina Export
 elif tabs == 'Export':
     st.header('{}'.format(tabs))
     if 'df' in st.session_state:
@@ -51,19 +50,3 @@ elif tabs == 'Export':
     else:
         st.write("What do you do?")
         st.write("Please, before all, load something in upload page.")
-
-
-        # st.download_button(
-        #     label="Download data as CSV",
-        #     data=convert_df_to_csv(dataframe),
-        #     file_name=file_output_name+'.csv',
-        #     mime='text/csv',
-        # )
-
-
-            # file_output_name = st.text_input(
-                # 'Which name for output file?',
-                # value=file_output_name
-                # )
-
-    # st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
