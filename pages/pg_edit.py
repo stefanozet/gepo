8ebdeb4500bc0fd8881d724f4324d54a97c05dc3
Stefano <stefano.rubini@gmail.com>
2023-01-27 12:40:06 +0100

    File di base

---
+++ pages/pg_edit.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import streamlit as st
+
+# st.set_page_config(
+#     layout="wide",
+#     page_title="Ciaoo",
+#     page_icon="👋",
+# )
+
+def edit(): # pd.DataFrame:
+
+    if 'df' in st.session_state:
+        df = st.session_state.df
+
+        tab1, tab2, tab3 = st.tabs(["Info", "Drop", "Elaborate"])
+    
+        with tab1:
+            ''
+        with tab2:
+            ''
+        with tab3:
+            ''
+        st.dataframe(st.session_state.df)
+
+    else:
+        st.write("What do you do?")
+        st.write("Please, before all, load something in upload page.")
+
+edit()
---
+++ pages/pg_upload.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import streamlit as st
+
+# file_ext = ['csv','xls']
+# cf_input_separator = ';', '|', ','
+
+def upload(file_ext, input_separator, rows_to_display):
+    
+    ext = 'xls'
+
+    uploaded_file = st.file_uploader(
+        "Choose a file", 
+        type = file_ext, 
+        accept_multiple_files=False, 
+        help="Load an file csv, xls or drang & drop it on the form."
+        )
+
+    col1, col2, col3, col4 = st.columns(4)
+
+    with col1:
+        cf_rows_to_display = st.number_input(
+            'Insert a number of rows to display', 
+            min_value=1, max_value=100, value=rows_to_display, step=10, format='%i'
+            )
+
+    with col2:
+        if uploaded_file and uploaded_file.type == 'text/csv':
+            separator = st.selectbox(
+                'Which is the separator?',
+                input_separator
+                )
+    
+    with col3:
+        if uploaded_file:
+            st.write("<div class='block_widget'>File type: {}</div>".format(uploaded_file.type), unsafe_allow_html=True)
+
+    with col4:
+        # st.button("Go to elaboration section", )
+        ''
+
+    if uploaded_file:
+        if uploaded_file.type == 'text/csv':
+            dataframe = pd.read_csv(uploaded_file, sep=separator)
+        else:
+            dataframe = pd.read_excel(uploaded_file)
+
+        st.dataframe(dataframe.head(cf_rows_to_display))
+        st.session_state.df = dataframe
